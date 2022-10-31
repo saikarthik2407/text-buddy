db3ee4497973399bdc248a33d1db7a669f817d4d
Venkata Sai Karthikeya <34571463+saikarthik2407@users.noreply.github.com>
2022-10-31 15:35:27 -0700

    initial commit of text buddy

---
+++ app/src/main/java/com/streax/textbuddy/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.streax.textbuddy.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.streax.textbuddy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
